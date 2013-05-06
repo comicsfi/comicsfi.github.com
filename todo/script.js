1f04d609b886b1bec83fecb6ab1c45a2edfff390
lesion <lesion@autistici.org>
2013-05-06 14:42:45 +0200

    lezione 10

--- todo/script.js
+++ todo/script.js
@@ -1,12 +1,54 @@
 
 $(document).ready(function(){
 
+  // static events bindings
   $('#add_button').on('click', app.add_todo );
+  $('#edit_button').on('click', app.edit_todo );
+
+  // dynamic events bindings
+  $('#main_todo_list').delegate('.task_title','click', app.toggle_todo_content );
+  $('#main_todo_list').delegate('.todo_edit','click', app.edit_todo_content );
+  //
+
+  // test event propagation/
+  /*
+  $('body').on('click', app.test_click );
+  $('#jQUi').on('click', app.test_click );
+  $('.panel').on('click', app.test_click );
+  $('#main_todo_list').on('click', app.test_click );*/
 
 });
 
 
 var app = {};
+
+app.edit_todo = function(){
+
+  console.log("Dentro edit_todo");
+  var id = $('#edit_task_id').val();
+  var title = $('#edit_task_title').val();
+  var content = $('#edit_task_content').val();
+
+  // modifico la struttura
+  todo.edit(id,title,content);
+  $.ui.loadContent('#main');
+
+};
+
+app.edit_todo_content = function(e){
+  var id= $(this).parent().attr('task_id');
+  console.log("Edit todo_content id: " + id);
+  var task = todo_list[id];
+  $('#edit #edit_task_title').val(task.title);
+  $('#edit #edit_task_content').val(task.content);
+  $('#edit #edit_task_id').val(id);
+  $.ui.loadContent('#edit');
+};
+
+app.test_click = function(e){
+  console.log(this.id);
+  if(this.id=='main') e.stopPropagation();
+};
 
 app.add_todo = function(){
   var title = $('#new_todo_title').val();
@@ -17,10 +59,25 @@ app.add_todo = function(){
     $.ui.popup({title:"Error",message:"<strong>Mandatory fields!</strong>",cancelOnly:true,cancelText:"Ok"});
     return;
   }
-
+  
   todo.add(title,content);
   $.ui.loadContent('#main');
 
+};
+
+
+app.toggle_todo_content= function(e){
+  console.log(this);
+  console.log("Son qui dentro");
+  var content_el = $(this).siblings('.task_content').eq(0);
+  content_el.toggle();
+};
+
+
+
+app.clickHandler = function(evt){
+  //console.log(evt);
+  return true;
 };
 
 
--- todo/todo.js
+++ todo/todo.js
@@ -1,14 +1,28 @@
+var todo_list = [];
+var todo = {};
 
 todo.add = function (title,content) {
 
-  var li = '<li>' + title + '</li>';
+  var id = todo_list.length;
 
   // add to structure
-  //todo_list.push( { title: title, content: content } ) ;
-  //
+  todo_list.push( { id: id, title: title, content: content } ) ;
 
-  console.log("Son qui e aggiungo " + li);
+  var content = '<div task_id="'+ id +
+                '" class="task"><button class="button todo_edit right">Edit</button><p class="task_title">' + 
+                title + '</p><p class="task_content">' + content + '<br></p></div>';
 
-  $('#main_todo_list').append(li);
+
+  $('#main_todo_list').append(content);
+  //$('#menu #menu_todo_list').append(li);
+  //$('.todo_title').on('click', app.toggle_todo_content );
 
 }
+
+todo.edit = function(id,title,content){
+  console.log(id);
+  todo_list[id] = { id: id, title: title, content: content };
+  $('.task[task_id="'+id+'"] .task_title').html(title);
+  $('.task[task_id="'+id+'"] .task_content').html(content);
+
+};
